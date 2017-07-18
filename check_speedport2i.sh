0a8a4413d68a63acd5df82d7f0418810dbf3a945
Nassos A. Michas <nassosmichas@me.com>
2017-07-19 00:17:15 +0300

    Remove non-numeric perf data

--- check_speedport2i.sh
+++ check_speedport2i.sh
@@ -8,13 +8,9 @@ Downstream_current_rate=$(grep -Po "<ParaName>Downstream_current_rate</ParaName>
 UpCrc_errors=$(grep -Po "<ParaName>UpCrc_errors</ParaName>.*?<ParaValue>\K(.*?)(?=</ParaValue>)" <<< "$STATUS")
 Downstream_attenuation=$(grep -Po "<ParaName>Downstream_attenuation</ParaName>.*?<ParaValue>\K(.*?)(?=</ParaValue>)" <<< "$STATUS")
 Fec_errors=$(grep -Po "<ParaName>Fec_errors</ParaName>.*?<ParaValue>\K(.*?)(?=</ParaValue>)" <<< "$STATUS")
-Status=$(grep -Po "<ParaName>Status</ParaName>.*?<ParaValue>\K(.*?)(?=</ParaValue>)" <<< "$STATUS")
 DownCrc_errors=$(grep -Po "<ParaName>DownCrc_errors</ParaName>.*?<ParaValue>\K(.*?)(?=</ParaValue>)" <<< "$STATUS")
 Atuc_fec_errors=$(grep -Po "<ParaName>Atuc_fec_errors</ParaName>.*?<ParaValue>\K(.*?)(?=</ParaValue>)" <<< "$STATUS")
 Upstream_attenuation=$(grep -Po "<ParaName>Upstream_attenuation</ParaName>.*?<ParaValue>\K(.*?)(?=</ParaValue>)" <<< "$STATUS")
-Module_type=$(grep -Po "<ParaName>Module_type</ParaName>.*?<ParaValue>\K(.*?)(?=</ParaValue>)" <<< "$STATUS")
-SoftwareVer=$(grep -Po "<ParaName>SoftwareVer</ParaName>.*?<ParaValue>\K(.*?)(?=</ParaValue>)" <<< "$STATUS")
-SerialNumber=$(grep -Po "<ParaName>SerialNumber</ParaName>.*?<ParaValue>\K(.*?)(?=</ParaValue>)" <<< "$STATUS")
 
 OUTPUT="ok | 
 Upstream_noise_margin=$Upstream_noise_margin
@@ -24,12 +20,8 @@ Downstream_current_rate=$Downstream_current_rate
 UpCrc_errors=$UpCrc_errors
 Downstream_attenuation=$Downstream_attenuation
 Fec_errors=$Fec_errors
-Status=$Status
 DownCrc_errors=$DownCrc_errors
 Atuc_fec_errors=$Atuc_fec_errors
-Upstream_attenuation=$Upstream_attenuation
-Module_type=$Module_type
-SoftwareVer=$SoftwareVer
-SerialNumber=$SerialNumber"
+Upstream_attenuation=$Upstream_attenuation"
 
 echo $OUTPUT
